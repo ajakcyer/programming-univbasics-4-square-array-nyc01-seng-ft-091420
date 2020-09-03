fba36b3504b3e5178c66a4dece1a133c3974db5a
Ajak Cyer <ajakcyer97@gmail.com>
2020-09-03 20:52:50 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   counter = 0
   new_array = []
   while counter < array.length do
-    new_array.push(array[counter] **2)
+    array[counter] ** 2
   end
     new_array
 end
